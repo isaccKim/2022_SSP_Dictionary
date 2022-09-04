0c94164186168aae6d5d34c7bd6999104fa3cb04
NotStoneSeok <kimsw0683@handong.ac.kr>
2022-09-04 22:14:39 +0900

    Add function in addWord Method

--- JAVA_CRUD/src/org/project/WordCRUD.java
+++ JAVA_CRUD/src/org/project/WordCRUD.java
@@ -24,7 +24,9 @@ public class WordCRUD implements ICRUD{
 	}
 	
 	public void addWord() {
-		
+		Word one = (Word)add();
+		list.add(one);
+		System.out.println("\n새 단어가 단어장에 추가되었습니다 !!!");
 		
 	}
 
