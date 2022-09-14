34248dc9fff6358d3ba36f9ca397c13eedbd7ef8
NotStoneSeok <kimsw0683@handong.ac.kr>
2022-09-14 22:11:51 +0900

    WordCRUD Updated

--- JAVA_CRUD/src/org/project/WordCRUD.java
+++ JAVA_CRUD/src/org/project/WordCRUD.java
@@ -165,6 +165,12 @@ public class WordCRUD implements ICRUD{
 		listAll(level);
 		
 	}
+	public void searchword() {
+		System.out.print("=> 원하는 단어는? ");
+		String keyword= sc.next();
+		listAll(keyword);
+		
+	}
 	
 }
  
